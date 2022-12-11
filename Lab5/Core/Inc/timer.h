f5c9ea21b9c95d4c40761fe22155dba1a875b775
Cong <congnguyenvan002@gmail.com>
2022-12-12 00:45:39 +0700

    lab5 done

---
+++ Lab5/Core/Inc/timer.h
@@ -0,0 +1,17 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 22, 2022
+ *      Author: shiba
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer0_flag;
+
+void setTimer0();
+void timerRun();
+
+
+#endif /* INC_TIMER_H_ */
---
+++ Lab5/Core/Src/timer.c
@@ -0,0 +1,29 @@
+/*
+ * timer.c
+ *
+ *  Created on: Nov 22, 2022
+ *      Author: shiba
+ */
+
+#include"timer.h"
+
+int TIMER_CYCLE = 10;
+
+int timer0_flag = 0;
+int timer0_counter = 0;
+
+void setTimer0(int duration){
+	timer0_counter = duration / TIMER_CYCLE;
+	timer0_flag = 0;
+}
+
+void timerRun(){
+	if(timer0_counter > 0){
+		timer0_counter--;
+		if(timer0_counter <= 0){
+			timer0_flag = 1;
+		}
+	}
+}
+
+
